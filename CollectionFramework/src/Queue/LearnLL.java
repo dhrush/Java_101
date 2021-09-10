35b167862ce9e09f2688fc63a1fbd41c0c424c85
dhrush <dhrush97@gmail.com>
2021-09-10 21:59:09 +0530

    Removed

---
+++ CollectionFramework/src/Queue/LearnLL.java
@@ -0,0 +1,14 @@
+package Queue;
+
+// import java.util.Queue;
+// import java.util.LinkedList;
+
+public class LearnLL {
+    public static void main(String[] args) {
+
+        System.out.println(System.getProperty("java.vendor"));
+        System.out.println(System.getProperty("java.vendor.url"));
+        System.out.println(System.getProperty("java.version"));
+
+    }
+}
--- LearnJava_0/src/Basics/App.java
+++
@@ -1,7 +0,0 @@
-package Basics;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello Devs, what's up !");
-    }
-}
