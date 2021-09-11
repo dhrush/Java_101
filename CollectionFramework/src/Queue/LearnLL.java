83183c00ae26368e78d3e79d346db6666b343bbb
dhrush <dhrush97@gmail.com>
2021-09-11 08:26:10 +0530

    Queue implemented

--- CollectionFramework/src/Queue/LearnLL.java
+++ CollectionFramework/src/Queue/LearnLL.java
@@ -1,14 +1,17 @@
 package Queue;
 
-// import java.util.Queue;
-// import java.util.LinkedList;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class LearnLL {
     public static void main(String[] args) {
-
-        System.out.println(System.getProperty("java.vendor"));
-        System.out.println(System.getProperty("java.vendor.url"));
-        System.out.println(System.getProperty("java.version"));
-
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(10);
+        q.offer(20);
+        q.offer(30); // offer-> insert elements to queue
+        System.out.println(q);
+        System.out.println(q.peek()); // peek -> returns element to be removed next
+        System.out.println(q.poll()); // poll -> deletes and returns element to be removed
+        System.out.println(q);
     }
 }
